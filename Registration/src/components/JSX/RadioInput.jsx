dd9651ae02f51be743d515912dfaa27fb55dfce6
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-01-15 00:47:16 +0530

    style: minor indents

--- Registration/src/components/JSX/RadioInput.jsx
+++ Registration/src/components/JSX/RadioInput.jsx
@@ -3,12 +3,16 @@ import RadioButtonGroup from "react-custom-radio-buttons-group";
 import "../CSS/Input.css";
 
 const RadioInput = props => {
-  const onChange = () => {}
+  const onChange = () => {};
 
   return (
     <div className="input">
       <label htmlFor={props.field}>{props.name}</label>
-      <RadioButtonGroup onChange={onChange} hide={false} values={["Male", "Female", "Others"]} />
+      <RadioButtonGroup
+        onChange={onChange}
+        hide={false}
+        values={["Male", "Female", "Others"]}
+      />
     </div>
   );
 };
--- Registration/src/components/JSX/TextInput.jsx
+++ Registration/src/components/JSX/TextInput.jsx
@@ -5,7 +5,12 @@ const TextInput = props => {
   return (
     <div className="input">
       <label htmlFor={props.field}>{props.name}</label>
-      <input className="text-input" type={props.type} id={props.field} name={props.field} />
+      <input
+        className="text-input"
+        type={props.type}
+        id={props.field}
+        name={props.field}
+      />
     </div>
   );
 };
