690dbda27b7b5e27da6d1bfe67ab7459bceaba2b
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-01-14 14:24:38 +0530

    build: add three.js, drei and gsap

---
+++ Home/src/components/JSX/Landing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Brain from "./Brain";
+import styles from "../CSS/landing.module.css";
+
+const Landing = () => {
+  return <Brain />;
+};
+
+export default Landing;
