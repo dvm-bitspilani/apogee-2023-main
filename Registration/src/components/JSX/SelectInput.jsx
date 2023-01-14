e96943bef528544bde6d4802235abc6a70670223
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-01-15 00:49:38 +0530

    style: react-styled component

--- Registration/src/components/JSX/SelectInput.jsx
+++ Registration/src/components/JSX/SelectInput.jsx
@@ -1,18 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 import Select from "react-select";
 import "../CSS/Input.css";
 
 const SelectInput = props => {
+  const colourStyles = {
+    control: base => ({ ...base, border: 0, boxShadow: "none" }),
+    option: (styles, { isSelected }) => {
+      return {
+        ...styles,
+        backgroundColor: isSelected ? "#51718d" : "#1b2025",
+        color: "#BFDFFFCC",
+        cursor: "pointer",
+        borderBottom: "2px solid #BFDFFFCC",
+
+        ":active": {
+          ...styles[":active"],
+          backgroundColor: "",
+        },
+      };
+    },
+    multiValue: styles => ({
+      ...styles,
+      backgroundColor: "#BFDFFFCC",
+    }),
+    multiValueLabel: styles => ({
+      ...styles,
+      color: "#1b2025",
+    }),
+    multiValueRemove: (styles, { data }) => ({
+      ...styles,
+      color: "#1b2025",
+      borderRadius: "0",
+
+      ":hover": {
+        backgroundColor: "#1b2025",
+        cursor: "pointer",
+        color: "#BFDFFFCC",
+      },
+    }),
+    menuPortal: base => ({ ...base, zIndex: 3, border: "none" }),
+    dropdownIndicator: base => ({
+      ...base,
+      color: "#BFDFFFCC",
+    }),
+    indicatorSeparator: () => {},
+  };
+
   return (
     <div className="input">
       <label htmlFor={props.field}>{props.name}</label>
       <Select
-        options={props.options}
         id={props.field}
+        closeMenuOnSelect={!props.isMulti}
+        options={props.options}
         name={props.field}
         isMulti={props.isMulti}
         classNamePrefix="react-select"
         placeholder=""
+        isClearable
+        menuPortalTarget={document.body}
+        styles={colourStyles}
       ></Select>
     </div>
   );
