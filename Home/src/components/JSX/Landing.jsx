b0871cc56932965a20288787be1a083aeffcb47d
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2023-01-18 03:07:54 +0530

    feat: add 2d 3d toggle and init responsiveness on lander

--- Home/src/components/JSX/Landing.jsx
+++ Home/src/components/JSX/Landing.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Brain from "./Brain";
 import Button from "./Button";
 import Countdown from "./Countdown";
 import styles from "../CSS/landing.module.css";
 import { Canvas } from "@react-three/fiber";
-
-import fb from "../../assets/fb.svg";
-import yt from "../../assets/youtube.svg";
-import ig from "../../assets/igBlue.svg";
+import FbIcon from "./FbIcon"
+import InstaIcon from "./InstaIcon"
+import YtIcon from "./YtIcon"
 import apogeelogo from "../../assets/apogeelogo.png";
+import Switch from "./Switch";
 
 function Landing() {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 450px)").matches
+  )
+
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 450px)")
+      .addEventListener('change', e => setMatches(e.matches));
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <Canvas id="canvas-wrapper">
@@ -19,17 +29,39 @@ function Landing() {
 
       <div className={styles.navbar}>
         <img src={apogeelogo} className={styles.apogeeLogo} />
-        <Button />
+        <Switch/>
+        {matches && <Button />}
       </div>
 
       <div className={styles.countDown}>
+      {!matches && <Button />}
         <Countdown />
       </div>
 
       <div className={styles.heroFoot}>
-        <img src={fb} />
-        <img src={ig} className={styles.blue} />
-        <img src={yt} />
+        <a
+          href="https://www.facebook.com/bitsapogee/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <FbIcon />
+        </a>
+
+        <a
+          href="https://instagram.com/bitsapogee"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <InstaIcon />
+        </a>
+
+        <a
+          href="https://www.youtube.com/@OasisBITS"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <YtIcon />
+        </a>
       </div>
     </div>
   );
