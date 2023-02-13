0b1e2e2ae4371f391768945fe666eeb3d0fb7518
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2023-02-13 19:09:23 +0530

    feat: loader

--- Home/src/App.jsx
+++ Home/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import BrainLabel from "./components/JSX/BrainLabel";
 import Events from "./components/JSX/Events";
 import Landing from "./components/JSX/Landing";
+import Loader from "./components/JSX/Loader";
 import ModalComp from "./components/JSX/ModalComp";
 import Modal from "./enums/Modal";
 
@@ -35,24 +36,33 @@ function App() {
   const [loaded, setLoaded] = useState(false);
   const [per, setPer] = useState(0);
 
-  let time=0,a=per*time/100;
   const onLoad = () => {
-    var width = 100, // width of a progress bar in percentage
-    perfData = window.performance.getEntriesByType("resource"); // The PerformanceTiming interface
-    let end = 0;
-    for(let i=0; i<perfData.length; i++) {
-      end = Math.max(end, perfData[i].responseEnd)
-    }
-    let EstimatedTime = end, // Calculated Estimated Time of Page Load which returns negative value.
-    time = EstimatedTime; //Converting EstimatedTime from miliseconds to seconds.\
+    let time = window.performance.getEntries("navigation")[0].loadEventEnd+1000;
+    let a = per*time/100;
     const int = setInterval(() => {
       a+=100
       setPer(Math.floor(a/time*100))
-      console.log(perfData);
-      console.log(EstimatedTime)
-      if(a/time*100 > 100) {clearInterval(int); setLoaded(true)}
+      if(a/time*100 >= 100) {clearInterval(int); setLoaded(true)}
     }, 100)
   }
+  // let time=0,a=per*time/100;
+  // const onLoad = () => {
+  //   var width = 100, // width of a progress bar in percentage
+  //   perfData = window.performance.getEntriesByType("resource"); // The PerformanceTiming interface
+  //   let end = 0;
+  //   for(let i=0; i<perfData.length; i++) {
+  //     end = Math.max(end, perfData[i].responseEnd)
+  //   }
+  //   let EstimatedTime = end, // Calculated Estimated Time of Page Load which returns negative value.
+  //   time = EstimatedTime; //Converting EstimatedTime from miliseconds to seconds.\
+  //   const int = setInterval(() => {
+  //     a+=100
+  //     setPer(Math.floor(a/time*100))
+  //     console.log(perfData);
+  //     console.log(EstimatedTime)
+  //     if(a/time*100 > 100) {clearInterval(int); setLoaded(true)}
+  //   }, 100)
+  // }
 
   useEffect(() => {
     onLoad();
@@ -60,15 +70,14 @@ function App() {
 
   return (
     <div className="App">
-      {!loaded ? (<div style={{width: '100vw', height: '100vh'}}>{`Loading... ${per}%`}</div>) :
-      (<ModalContext.Provider value={context}>
+      {!loaded ? <Loader percent={per} /> : <></> }
+      <ModalContext.Provider value={context}>
         <Landing />
         {displayModal ? <ModalComp /> : <></>}
         {labels.event ? <BrainLabel modal={Modal.Event} /> : <></>}
         {labels.contact ? <BrainLabel modal={Modal.Contact} /> : <></>}
-      </ModalContext.Provider>)
-}
-
+      </ModalContext.Provider>
+      {/* <Loader /> */}
     </div>
   );
 }
