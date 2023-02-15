95d71f33b663b0c31ab7e8d0e8da181d74e953ac
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-02-16 00:08:07 +0530

    fix: yt icon and glow

--- Home/src/components/JSX/Landing.jsx
+++ Home/src/components/JSX/Landing.jsx
@@ -6,12 +6,40 @@ import LandingComp from "./LandingComp";
 
 
 function Landing(props) {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 450px)").matches
+  );
+
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 450px)")
+      .addEventListener("change", e => setMatches(e.matches));
+
+    setLinks(
+      ICONS.map((e, i) => (
+        <a
+          key={i}
+          href={e[1]}
+          style={i == 2 ? { transform: "translateX(2px) scale(1.2)" } : {}}
+          rel="noreferrer"
+          target="_blank"
+        >
+          {e[0]}
+        </a>
+      ))
+    );
+  }, []);
 
   let contact = document.getElementById("contactUs");
   const scroll = () => contact.scrollIntoView({ behavior: "smooth" });
 
   return (
-    <div className={styles.wrapper} style={props.loaded ? {display: 'block'} : {display: 'none'}}>
+    <div
+      className={styles.wrapper}
+      style={props.loaded ? { display: "block" } : { display: "none" }}
+    >
       <Canvas onContextMenu={e => e.preventDefault()} id="canvas-wrapper">
         <Brain />
       </Canvas>
