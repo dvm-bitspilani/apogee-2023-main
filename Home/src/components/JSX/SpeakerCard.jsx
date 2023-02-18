0aba84c861ad218958bc137d28f44a2a91e13c59
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2023-02-19 01:33:12 +0530

    feat: dots added in speakers card

--- Home/src/components/JSX/SpeakerCard.jsx
+++ Home/src/components/JSX/SpeakerCard.jsx
@@ -5,11 +5,27 @@ import Watch from "./Watch";
 
 export default function SpeakerCard(props) {
 
+  const Dots = () => {
+    return(
+      <div className="dots">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+    )
+  }
+
   return (
     <div className="speakerCard">
       <div className="speakerCont">
         <div className="speakerName">{props.data.name}</div>
         <div className="speakerPos">{props.data.pos}</div>
+        <Dots />
       </div>
       <div className="speakerImg">
         <img src={props.data.img} alt="" />
