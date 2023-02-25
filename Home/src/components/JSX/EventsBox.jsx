a7e313cde8992577fa10424e8d175cfed31c0d10
CoderVaibhavS <vaibhavsingla123.vs@gmail.com>
2023-02-26 01:30:54 +0530

    minor code improvement

--- Home/src/components/JSX/EventsBox.jsx
+++ Home/src/components/JSX/EventsBox.jsx
@@ -7,16 +7,13 @@ export default function EventsBox() {
   return (
     <div className={styles.box}>
       <div className={styles.boxLeft}>
-        {/* <img className={styles.boxLImg} src={boxL} alt="" /> */}
       </div>
       <div className={styles.boxRight}>
-        {/* <img className={styles.boxRImg} src={boxR} alt="" /> */}
         <div className={styles.bars}>
-            <div className={styles.bar} style={{"--delay": `${Math.random()}s`, "--duration": `${Math.random()*2 + 5}s`}}></div>
-            <div className={styles.bar} style={{"--delay": `${Math.random()}s`, "--duration": `${Math.random()*2 + 5}s`}}></div>
-            <div className={styles.bar} style={{"--delay": `${Math.random()}s`, "--duration": `${Math.random()*2 + 5}s`}}></div>
-            <div className={styles.bar} style={{"--delay": `${Math.random()}s`, "--duration": `${Math.random()*2 + 5}s`}}></div>
-            <div className={styles.bar} style={{"--delay": `${Math.random()}s`, "--duration": `${Math.random()*2 + 5}s`}}></div>
+            {Array(5)
+            .fill()
+            .map(idx => (<div key={idx} className={styles.bar} style={{"--delay": `${Math.random()}s`, "--duration": `${Math.random()*2 + 5}s`}}></div>
+            ))}
         </div>
       </div>
     </div>
