5ef7d6b144e532ae01d9a78a0261bf17e7d4f72d
Jay Goyal <goyal.jay2003@gmail.com>
2023-03-24 23:45:03 +0530

    Hamburger animation

--- Home/src/SponsorList.jsx
+++ Home/src/SponsorList.jsx
@@ -1,117 +1,140 @@
-export const sponsors = [{
-    "Partner": "Gaming Partner",
-    "PartnerName": "ASUS",
-    "Link": "https://www.asus.com/in/",
-    "Logo": "../logos/ASUS-Logo.png"
-}, {
-    "Partner": "Official Engineering Design Partner",
-    "PartnerName": "Autodesk",
-    "Link": "https://www.autodesk.in/",
-    "Logo": ""
-
-}, {
-    "Partner": "Official Grooming Partner",
-    "PartnerName": "Beardo",
-    "Link": "https://beardo.in",
-    "Logo": "../logos/beardo.png"
-}, {
-    "Partner": "Official Community Partner",
-    "PartnerName": "BuidlersTribe",
-    "Link": "https://buidlerstribe.com/",
-    "Logo": ""
-}, {
-    "Partner": "Official Beverage Partner",
-    "PartnerName": "Continental Coffee",
-    "Link": "https://www.cclproducts.com/",
-    "Logo": "../logos/this logo size 1.25 X-02 (2).jpg"
-}, {
-    "Partner": "Official CaseComp Partner",
-    "PartnerName": "E-Learnmarkets",
-    "Link": "https://www.elearnmarkets.com/",
-    "Logo": "../logos/WhatsApp Image 2023-03-21 at 10.45.46 AM.jpeg"
-}, {
-    "Partner": "Official Financial Wellness Partner",
-    "PartnerName": "FinMapp",
-    "Link": "https://www.finmapp.com/",
-    "Logo": "../logos/logo-with-tagline.png"
-}, {
-    "Partner": "Offical Fragrance Partner",
-    "PartnerName": "French Factor",
-    "Link": "https://french-factor.com/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Banking Partner",
-    "PartnerName": "HDFC",
-    "Link": "https://www.hdfcbank.com/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Energy Coffee Partner",
-    "PartnerName": "Hell Energy",
-    "Link": "https://www.hellenergy.com/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Knowledge Partner",
-    "PartnerName": "LWT",
-    "Link": "https://learningwhiletravelling.com/",
-    "Logo": "../logos/001_LWT_logo.png"
-}, {
-    "Partner": "Offical Mentorship Partner",
-    "PartnerName": "Metvy",
-    "Link": "https://www.metvy.com/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Powered by Sponsor",
-    "PartnerName": "Quick Smart Wash<",
-    "Link": "https://www.quicksmartwash.com/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Event Partner",
-    "PartnerName": "Samaro",
-    "Link": "https://www.samaro.app/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Refreshment Partner",
-    "PartnerName": "Skippi",
-    "Link": "https://skippi.in/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Music Production Partner",
-    "PartnerName": "SoundIdeaz",
-    "Link": "https://www.soundideazacademy.in/",
-    "Logo": "../logos/Soundideaz.png"
-}, {
-    "Partner": "Offical Snacking Partner",
-    "PartnerName": "Too Yumm",
-    "Link": "https://www.tooyumm.com/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Gold Sponsor",
-    "PartnerName": "Walmart",
-    "Link": "https://www.walmart.com/",
-    "Logo": "../logos/Walmart_GT_Logo_Walmart Global Tech Stacked.png"
-}, {
-    "Partner": "Associate Partner",
-    "PartnerName": "Google Developers",
-    "Link": "https://developers.google.com/",
-    "Logo": "../logos/google_developers_PNG.png"
-}, {
-    "Partner": "Offical Sponsor",
-    "PartnerName": "Hubhoppper",
-    "Link": "https://hubhopper.com/",
-    "Logo": "../logos/Hubhopper.png"
-}, {
-    "Partner": ">Offical Social Media Partner",
-    "PartnerName": "https://bereal-app.typeform.com/BitsPilani",
-    "Link": "BeReal",
-    "Logo": "../logos/WhatsApp Image 2023-03-22 at 4.08.32 PM.jpeg"
-}, {
-    "Partner": "Offical Drone Technology Partner",
-    "PartnerName": "BotLab Dynamics",
-    "Link": "https://botlabdynamics.com/",
-    "Logo": ""
-}, {
-    "Partner": "Offical Travel Partner",
-    "PartnerName": "https://www.easemytrip.com/",
-    "Link": "EaseMyTrip",
-    "Logo": "../logos/White Logo.png"
-}]
+export const sponsors = [
+  {
+    Partner: "Gaming Partner",
+    PartnerName: "ASUS",
+    Link: "https://www.asus.com/in/",
+    Logo: "../logos/ASUS-Logo.png",
+  },
+  {
+    Partner: "Official Engineering Design Partner",
+    PartnerName: "Autodesk",
+    Link: "https://www.autodesk.in/",
+    Logo: "",
+  },
+  {
+    Partner: "Official Grooming Partner",
+    PartnerName: "Beardo",
+    Link: "https://beardo.in",
+    Logo: "../logos/beardo.png",
+  },
+  {
+    Partner: "Official Community Partner",
+    PartnerName: "BuidlersTribe",
+    Link: "https://buidlerstribe.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Official Beverage Partner",
+    PartnerName: "Continental Coffee",
+    Link: "https://www.cclproducts.com/",
+    Logo: "../logos/this logo size 1.25 X-02 (2).jpg",
+  },
+  {
+    Partner: "Official CaseComp Partner",
+    PartnerName: "E-Learnmarkets",
+    Link: "https://www.elearnmarkets.com/",
+    Logo: "../logos/WhatsApp Image 2023-03-21 at 10.45.46 AM.jpeg",
+  },
+  {
+    Partner: "Official Financial Wellness Partner",
+    PartnerName: "FinMapp",
+    Link: "https://www.finmapp.com/",
+    Logo: "../logos/logo-with-tagline.png",
+  },
+  {
+    Partner: "Offical Fragrance Partner",
+    PartnerName: "French Factor",
+    Link: "https://french-factor.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Banking Partner",
+    PartnerName: "HDFC",
+    Link: "https://www.hdfcbank.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Energy Coffee Partner",
+    PartnerName: "Hell Energy",
+    Link: "https://www.hellenergy.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Knowledge Partner",
+    PartnerName: "LWT",
+    Link: "https://learningwhiletravelling.com/",
+    Logo: "../logos/001_LWT_logo.png",
+  },
+  {
+    Partner: "Offical Mentorship Partner",
+    PartnerName: "Metvy",
+    Link: "https://www.metvy.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Powered by Sponsor",
+    PartnerName: "Quick Smart Wash<",
+    Link: "https://www.quicksmartwash.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Event Partner",
+    PartnerName: "Samaro",
+    Link: "https://www.samaro.app/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Refreshment Partner",
+    PartnerName: "Skippi",
+    Link: "https://skippi.in/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Music Production Partner",
+    PartnerName: "SoundIdeaz",
+    Link: "https://www.soundideazacademy.in/",
+    Logo: "../logos/Soundideaz.png",
+  },
+  {
+    Partner: "Offical Snacking Partner",
+    PartnerName: "Too Yumm",
+    Link: "https://www.tooyumm.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Gold Sponsor",
+    PartnerName: "Walmart",
+    Link: "https://www.walmart.com/",
+    Logo: "../logos/Walmart_GT_Logo_Walmart Global Tech Stacked.png",
+  },
+  {
+    Partner: "Associate Partner",
+    PartnerName: "Google Developers",
+    Link: "https://developers.google.com/",
+    Logo: "../logos/google_developers_PNG.png",
+  },
+  {
+    Partner: "Offical Sponsor",
+    PartnerName: "Hubhoppper",
+    Link: "https://hubhopper.com/",
+    Logo: "../logos/Hubhopper.png",
+  },
+  {
+    Partner: ">Offical Social Media Partner",
+    PartnerName: "https://bereal-app.typeform.com/BitsPilani",
+    Link: "BeReal",
+    Logo: "../logos/WhatsApp Image 2023-03-22 at 4.08.32 PM.jpeg",
+  },
+  {
+    Partner: "Offical Drone Technology Partner",
+    PartnerName: "BotLab Dynamics",
+    Link: "https://botlabdynamics.com/",
+    Logo: "",
+  },
+  {
+    Partner: "Offical Travel Partner",
+    PartnerName: "https://www.easemytrip.com/",
+    Link: "EaseMyTrip",
+    Logo: "../logos/White Logo.png",
+  },
+];
--- Home/src/components/JSX/Sponsors.jsx
+++ Home/src/components/JSX/Sponsors.jsx
@@ -5,26 +5,29 @@ import { sponsors } from "../../SponsorList";
 import { useNavigate } from "react-router-dom";
 
 function Sponsors() {
+  const navigate = useNavigate();
+  const navigateBack = () => navigate("/");
 
-    const navigate = useNavigate()
-    const navigateBack = () => navigate('/');
-
-    const cards = sponsors.map((sponsor) => {
-        return (
-            <SponsCard name={sponsor.PartnerName} title={sponsor.Partner} link={sponsor.Link} img={sponsor.Logo} />
-        );
-    });
-
+  const cards = sponsors.map(sponsor => {
     return (
-        <div className={SponzCSS.container}>
-            <div onClick={navigateBack} className="backBtn">
-            </div>
-            <div>
-                <div className={SponzCSS.title}>SPONSORS</div>
-                {cards}
-            </div>
-        </div>
+      <SponsCard
+        name={sponsor.PartnerName}
+        title={sponsor.Partner}
+        link={sponsor.Link}
+        img={sponsor.Logo}
+      />
     );
+  });
+
+  return (
+    <div className={SponzCSS.container}>
+      <div onClick={navigateBack} className="backBtn"></div>
+      <div>
+        <div className={SponzCSS.title}>SPONSORS</div>
+        {cards}
+      </div>
+    </div>
+  );
 }
 
 export default Sponsors;
