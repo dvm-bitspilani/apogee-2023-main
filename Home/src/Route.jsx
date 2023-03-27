279db55dd5205d09411fca6abdff61d20f272af3
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-03-27 17:00:17 +0530

    chore: server at Route

---
+++ Home/src/Route.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Armageddon from "./components/JSX/Armageddon";
+import Error from "./components/JSX/Error";
+import Sponsors from "./components/JSX/Sponsors";
+import "./index.css";
+
+const Route = () => {
+  return (
+    <RouterProvider
+      router={createBrowserRouter([
+        {
+          path: "/",
+          element: <App />,
+          errorElement: <Error />,
+        },
+        {
+          path: "/armageddon",
+          element: <Armageddon />,
+          errorElement: <Error />,
+        },
+        {
+          path: "/sponsors",
+          element: <Sponsors />,
+          errorElement: <Error />,
+        },
+      ])}
+    />
+  );
+};
+
+export default Route;
--- Home/src/main.jsx
+++ Home/src/main.jsx
@@ -1,32 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import Sponsors from "./components/JSX/Sponsors";
-import Armageddon from "./components/JSX/Armageddon";
-import Error from "./components/JSX/Error";
-
-const ROUTER = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/armageddon",
-    element: <Armageddon />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/sponsors",
-    element: <Sponsors />,
-    errorElement: <Error />,
-  },
-]);
+import Route from "./Route";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={ROUTER} />
+    <Route />
   </React.StrictMode>
 );
